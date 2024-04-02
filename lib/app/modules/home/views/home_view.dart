98d33a3cc100b2eb54f35b9a80534bfbfa6d19a4
Jovan Josafat <jovan.josafat@gmail.com>
2024-04-03 01:26:32 +0700

    updated

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:jantung_app/app/modules/home/views/patients_card.dart';
 import 'package:jantung_app/app/widgets/default_button.dart';
 import 'package:jantung_app/core/utils/size_config.dart';
 import '../controllers/home_controller.dart';
@@ -8,6 +9,7 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
@@ -41,70 +43,7 @@ class HomeView extends GetView<HomeController> {
                         return Container(
                             child: Row(
                           mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(4),
-                              ),
-                              clipBehavior: Clip.antiAliasWithSaveLayer,
-                              child: Container(
-                                padding: EdgeInsets.all(15.0),
-                                width: SizeConfig.screenWidth * 0.88,
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Image.asset(
-                                      controller.getImage(index).toString(),
-                                      height: 100,
-                                      width: 100,
-                                      fit: BoxFit.cover,
-                                    ),
-                                    SizedBox(
-                                        width: getProportionateScreenWidth(20)),
-                                    Expanded(
-                                      child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            SizedBox(
-                                                height:
-                                                    getProportionateScreenHeight(
-                                                        20)),
-                                            Text(
-                                              '${controller.listPatient[index]['patient_name']}',
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                          ]),
-                                    ),
-                                    Expanded(
-                                      child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.end,
-                                          children: [
-                                            SizedBox(
-                                                height:
-                                                    getProportionateScreenHeight(
-                                                        55)),
-                                            ElevatedButton(
-                                              onPressed: () {},
-                                              child: Icon(Icons
-                                                  .arrow_forward_ios_rounded), // Empty child
-                                              style: ElevatedButton.styleFrom(
-                                                shape: RoundedRectangleBorder(
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          50.0), // Adjust the value for the desired roundness
-                                                ),
-                                              ),
-                                            ),
-                                          ]),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
+                          children: [PatientCard(index: index)],
                         ));
                       },
                     ),
---
+++ lib/app/modules/home/views/patients_card.dart
@@ -0,0 +1,65 @@
+import 'package:jantung_app/app/modules/home/controllers/home_controller.dart';
+import 'package:jantung_app/app/widgets/custom_tff.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:jantung_app/core/utils/size_config.dart';
+
+class PatientCard extends StatelessWidget {
+  final controller = Get.find<HomeController>();
+  final index;
+  PatientCard({super.key, this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4),
+      ),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Container(
+        padding: EdgeInsets.all(15.0),
+        width: SizeConfig.screenWidth * 0.88,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              controller.getImage(index).toString(),
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(width: getProportionateScreenWidth(20)),
+            Expanded(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: getProportionateScreenHeight(20)),
+                    Text(
+                      '${controller.listPatient[index]['patient_name']}',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ]),
+            ),
+            Expanded(
+              child:
+                  Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                SizedBox(height: getProportionateScreenHeight(55)),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.arrow_forward_ios_rounded), // Empty child
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          50.0), // Adjust the value for the desired roundness
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
